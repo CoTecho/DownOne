e6b6e1bfe2a36bcdb347596d73d0dc1405921413
CoTecho <Co@std.uestc.edu.cn>
2023-05-14 17:29:02 +0800

    添加封面工具

---
+++ tools/albumcover.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""ChatGPT生成的一段修改封面的代码"""
+from pathlib import Path
+import subprocess
+
+
+def AutoAddCover(path):
+    """输入文件夹，自动将文件夹下的音频文件封面替换为文件夹同名的jpg文件"""
+    folder_path = Path(path)
+    audio_ext = ['.mp3', '.wav']
+    jpg_ext = '.jpg'
+
+    # 循环处理文件夹中的每个MP3文件
+    for file_path in folder_path.glob('*'):
+        if file_path.is_file() and file_path.suffix in audio_ext:
+            # 构造封面文件路径
+            cover_path = folder_path.joinpath(folder_path.name + jpg_ext)
+            # 构造临时文件路径
+            temp_output_path = file_path.with_suffix('.temp' + file_path.suffix)
+
+            # 构造FFmpeg命令
+            cmd = ['ffmpeg', '-i', str(file_path), '-i', str(cover_path), '-map', '0', '-map', '1',
+                   '-metadata:s:v', 'title="Album cover"', '-metadata:s:v', 'comment="Cover (front)"',
+                   '-c', 'copy', '-y', str(temp_output_path)]
+
+            # 调用FFmpeg命令
+            subprocess.run(cmd)
+
+            # 删除原始文件
+            file_path.unlink()
+
+            # 将临时文件重命名为原始文件
+            temp_output_path.rename(file_path)
